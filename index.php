43fd00513a071721252845cba268a5f43bc21000
creanpie <ss1041310413@gmail.com>
2021-03-16 17:23:29 +0800

    css

--- index.php
+++ index.php
@@ -2,12 +2,27 @@
 <html>
 
 <head>	
-<title>learning file</title>
+	<title>learning file</title>
+	<style type="text/css">
+		body {
+		  background-color: lightblue;
+		}
+
+		h1 {
+		  color: white;
+		  text-align: center;
+		}
+
+		p {
+		  font-family: verdana;
+		  font-size: 20px;
+		}
+	</style>>
 </head>
 
 <body>
-<h1>school email</h1>
-<p>C107107142@nkust.edu.tw</p>
+	<h1>school e-mail</h1>
+	<p>C107107142@nkust.edu.tw</p>
 </body>
 
 </html>
