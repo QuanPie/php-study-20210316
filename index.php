e4ae832c698840a484d6eb1bc62c2fe097e89a23
creanpie <ss1041310413@gmail.com>
2021-03-23 17:16:41 +0800

    commitcommit

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html>
 
 <head>	
-	<title>learning file</title>
+	<title>楊長權</title>
 	<style type="text/css">
 		body {
-		  background-color: lightblue;
+			font-family: 微軟正黑體;
+		  	background-color: lightblue;
 		}
 
 		h1 {
@@ -21,8 +22,18 @@
 </head>
 
 <body>
-	<h1>school e-mail</h1>
-	<p>C107107142@nkust.edu.tw</p>
+	<h1>楊長權</h1>
+	<button><a href="/mysite/">首頁</a></button>
+	<button><a href="http://www.she.nkfust.edu.tw/">環安系</a></button>
+	<button><a href="http://tzukuan.nkust.org">苗圃計畫官網</a></button>
+	<select onchange='location.href="/mysite/"+this.value'>
+		<option value="0323-1.php">請選擇要執行的練習</option>
+		<option value="0323-1.php">0323-1</option>
+		<option value="0323-2.php">0323-2</option>
+	</select>
+	<hr>
+
+	<p>school e-mail:C107107142@nkust.edu.tw</p>
 </body>
 
 </html>
